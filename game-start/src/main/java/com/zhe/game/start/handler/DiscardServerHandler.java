580ce0736f3f8a3faa8a88069b3d3bba3a8d5fc0
zh-e <zz950702@163.com>
2022-07-17 22:04:06 +0800

    init

---
+++ game-start/src/main/java/com/zhe/game/start/handler/DiscardServerHandler.java
@@ -0,0 +1,22 @@
+package com.zhe.game.start.handler;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ctx.write(msg);
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+}
