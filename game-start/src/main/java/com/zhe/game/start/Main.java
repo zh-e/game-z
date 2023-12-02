e8c042958205a4c4a3a958ecfaa044d483c7c2c5
zh-e <zz950702@163.com>
2023-12-02 22:04:06 +0800

    init

---
+++ game-start/src/main/java/com/zhe/game/start/Main.java
@@ -0,0 +1,10 @@
+package com.zhe.game.start;
+
+public class Main {
+
+    public static void main(String[] args) {
+       new DiscardServer(8080).run();
+        System.out.println("start over");
+    }
+
+}
