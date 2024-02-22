6efa19b621c7128e45986db8d38a916e851f709d
zh-e <zz950702@163.com>
2024-02-22 22:54:51 +0800

    日志配置

--- game-start/src/main/java/com/z/game/start/Application.java
+++ game-start/src/main/java/com/z/game/start/Application.java
@@ -4,16 +4,20 @@ import com.z.game.start.config.ConfigManager;
 import com.z.game.start.config.ServerConfig;
 import com.z.game.start.core.Node;
 import com.z.game.start.netty.WsServer;
-import com.z.game.start.util.JsonUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-public class Main {
+
+public class Application {
+
+    private static final Logger LOGGER = LogManager.getLogger(Application.class);
 
     public static void main(String[] args) {
+        String nodeId = System.getProperty("nodeId", "word0");
+        LOGGER.info("application start node={}", nodeId);
+
         //初始化配置
         ConfigManager.init();
-
-        String nodeId = System.getProperty("nodeId", "word0");
-
         Node node = new Node(nodeId, "");
 
 
