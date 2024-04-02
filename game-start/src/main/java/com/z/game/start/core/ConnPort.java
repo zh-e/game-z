36e0e7be22673eafbf540b983fef92169a28876c
zh-e <zz950702@163.com>
2024-04-02 20:03:15 +0800

    鉴权逻辑

--- game-start/src/main/java/com/z/game/start/core/ConnPort.java
+++ game-start/src/main/java/com/z/game/start/core/ConnPort.java
@@ -1,6 +1,14 @@
 package com.z.game.start.core;
 
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * 链接port
+ * 仅处理链接事件
+ */
 public class ConnPort extends Port {
+
+    private static final ConcurrentHashMap<String, ConnService> connServiceMap = new ConcurrentHashMap<>();
 
     public ConnPort(String portId) {
         super(portId);
---
+++ game-start/src/main/java/com/z/game/start/core/ConnService.java
@@ -0,0 +1,10 @@
+package com.z.game.start.core;
+
+/**
+ * 链接时间处理
+ */
+public class ConnService extends Service{
+
+
+
+}
