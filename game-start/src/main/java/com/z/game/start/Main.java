4b71cec71ac9a47790bd16901b592077415f9990
zh-e <zz950702@163.com>
2024-04-21 16:53:54 +0800

    todo

--- game-start/src/main/java/com/z/game/start/Main.java
+++ game-start/src/main/java/com/z/game/start/Main.java
@@ -1,9 +1,13 @@
 package com.z.game.start;
 
+import com.z.game.start.netty.WsServer;
+
 public class Main {
 
     public static void main(String[] args) {
+        new WsServer(8888).start();
 
     }
+
 
 }
--- game-start/src/main/java/com/z/game/start/netty/handler/DefaultWsHandlerManager.java
+++ game-start/src/main/java/com/z/game/start/netty/handler/DefaultWsHandlerManager.java
@@ -1,6 +1,12 @@
 package com.z.game.start.netty.handler;
 
 import io.netty.channel.ChannelPipeline;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import io.netty.handler.timeout.IdleStateHandler;
+
+import java.util.concurrent.TimeUnit;
 
 public class DefaultWsHandlerManager extends HandlerManager{
 
@@ -8,17 +14,22 @@ public class DefaultWsHandlerManager extends HandlerManager{
 
     @Override
     public void addProtocolHandlers(ChannelPipeline pipeline) {
-
+        pipeline.addLast(new HttpServerCodec());
+        pipeline.addLast(new HttpObjectAggregator(65536));
+        pipeline.addLast(new WebSocketServerProtocolHandler("/ws", true));
+        pipeline.addLast(new IdleStateHandler(1, 0, 0, TimeUnit.SECONDS));
+        pipeline.addLast(new WsEncoder());
+        pipeline.addLast(new WsDecoder());
     }
 
     @Override
     public void addMetricsHandlers(ChannelPipeline pipeline) {
-
+        //TODO 监控
     }
 
     @Override
     public void addBizHandlers(ChannelPipeline pipeline) {
-
+        pipeline.addLast(new WsServerHandler());
     }
 
 }
