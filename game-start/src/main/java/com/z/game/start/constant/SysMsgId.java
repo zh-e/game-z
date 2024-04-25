fad5c1dd9ec3b9b0d39bfbc9adbd2a35e1dc7398
zh-e <zz950702@163.com>
2024-04-25 22:57:10 +0800

    登陆逻辑处理

--- game-start/src/main/java/com/z/game/start/constant/SysMsgId.java
+++ game-start/src/main/java/com/z/game/start/constant/SysMsgId.java
@@ -2,8 +2,9 @@ package com.z.game.start.constant;
 
 public class SysMsgId {
 
+   public static final int HEART_BEAT = 1000;
+
    public static final int LOGIN = 1001;
 
-   public static final int HEART_BEAT = 1000;
 
 }
