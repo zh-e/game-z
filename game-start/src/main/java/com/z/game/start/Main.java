101971df4ae2ce9c1a64a5cf536460b1dc7afb67
zh-e <zz950702@163.com>
2024-05-03 20:01:51 +0800

    配置读取

--- game-start/src/main/java/com/z/game/start/Main.java
+++ game-start/src/main/java/com/z/game/start/Main.java
@@ -1,11 +1,22 @@
 package com.z.game.start;
 
+import com.z.game.start.config.ConfigManager;
+import com.z.game.start.config.ServerConfig;
 import com.z.game.start.netty.WsServer;
+import com.z.game.start.util.JsonUtils;
 
 public class Main {
 
     public static void main(String[] args) {
-        new WsServer(8888).start();
+        //初始化配置
+        ConfigManager.init();
+
+        ServerConfig serverConfig = ConfigManager.getServerConfig();
+
+
+        System.out.println(JsonUtils.toJson(serverConfig));
+
+        new WsServer(serverConfig.getNettyBossGroupSize(), serverConfig.getNettyWorkerGroupSize(), serverConfig.getNettyPort()).start();
 
     }
 
