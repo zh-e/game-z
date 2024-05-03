4d6c38f397c19f202794a63cf486205878244532
zh-e <zz950702@163.com>
2024-05-03 21:40:11 +0800

    核心类初定义

---
+++ game-start/src/main/java/com/z/game/start/core/Service.java
@@ -0,0 +1,20 @@
+package com.z.game.start.core;
+
+import com.z.game.start.core.interfaces.Actuator;
+
+public class Service implements Actuator {
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public void runOnce() {
+
+    }
+}
