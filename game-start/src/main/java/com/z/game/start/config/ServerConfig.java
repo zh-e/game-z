dd856272168e780477fa21d342da80e8fe1940b5
zh-e <zz950702@163.com>
2025-05-03 20:01:51 +0800

    配置读取

---
+++ game-start/src/main/java/com/z/game/start/config/ServerConfig.java
@@ -0,0 +1,20 @@
+package com.z.game.start.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ServerConfig {
+
+    private int nettyPort = 8888;
+
+    private int nettyBossGroupSize = 1;
+
+    private int nettyWorkerGroupSize = 16;
+
+}
